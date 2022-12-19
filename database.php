72b6d29dfac7eaa42d9e65ee1ef0f69a98e980c7
tahrima21 <tahrima21@gmail.com>
2022-12-20 05:06:18 +0600

    few issues remaining

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "techrev" );
-    if ($conn-> connect_error){
-        die("Connection failed:".$conn-> connect_error);
-    }
-    
-    
-    
-    ?>
--- suggestions.js
+++
@@ -1,6 +0,0 @@
-let suggestions = [
-    "Samsung Galaxy S8",
-    "Samsung Smart TV",
-    "Logitech Keyboard",
-    "Nvidia GeForce GTX 960",
-];
